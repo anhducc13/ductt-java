94fd739452ee27ca270d9bad71db97914dcbe9d6
anhducc13 <trantienduc10@gmai.com>
2018-11-10 23:46:05 +0700

    updateLap10

--- OOP/Week10/src/week10/Prac2.java
+++ OOP/Week10/src/week10/Prac2.java
@@ -1,5 +1,39 @@
 package week10;
 
+import java.util.Random;
+
 public class Prac2 {
     
+    public static double [] randomArr(int n, double min, double max) {
+        double [] arr = new double[n];
+        Random r = new Random();
+        for(int i = 0; i < n; i++) {
+            double num = min + (max - min) * r.nextDouble();
+            arr[i] = Math.round(num*100)/100.00;
+        }
+        
+        return arr;
+    }
+    
+    public static void bubbleSort(double [] arr) {
+        int length = arr.length;
+        for(int i = 0; i < length-1; i++) {
+            for(int j = i+ 1; j < length; j++) {
+                if(arr[i] > arr[j]) {
+                    double tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        double [] arr = randomArr(1000, -100, 100);
+        bubbleSort(arr);
+        for(double ele : arr) {
+            System.out.println(ele);
+        }
+        
+    }
 }
